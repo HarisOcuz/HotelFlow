55c4839957b9a11adbfb884cea23f73b7436b1da
HarisOcuz <hariso94@outlook.com>
2025-12-16 02:46:51 +0100

    Login seite - fertig

---
+++ src/dashboard.js
@@ -0,0 +1,61 @@
+export default Dashboard;
+
+function Dashboard() {
+  return (
+    <div>
+      <NavBar />
+
+      <div className="main-window">
+        <SideBar />
+        <MainDashboard />
+      </div>
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav className="navigation-bar">
+      <h3>Wilkommen zurück Haris</h3>
+
+      <img src="\Hotel_Flow__1_-removebg-preview.png" alt="Logo" />
+    </nav>
+  );
+}
+
+function SideBar() {
+  return (
+    <div className="side-bar">
+      <Button children={"Anreisen"} />
+      <Button children={"Abreisen"} />
+      <Button children={"Im Haus"} />
+      <Button children={"Mitarbeiter"} />
+      <Button children={"Zimmer"} />
+      <Widgets />
+    </div>
+  );
+}
+
+function Button({ children }) {
+  return <button className="btn">{children}</button>;
+}
+
+function Widgets() {
+  return (
+    <div className="widget-container">
+      <h4>Widgets</h4>
+      <button className="btn">Rechner</button>
+      <button className="btn">Kalendar</button>
+    </div>
+  );
+}
+
+function MainDashboard() {
+  return (
+    <div className="main-dashboard">
+      <div className="dashboard-info">
+        <span>Freie/Saubere Zimmer</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/login.js
@@ -0,0 +1,97 @@
+import { useState, useRef } from "react";
+
+export default Login;
+
+function Login() {
+  return (
+    <div className="container">
+      <LoginForm />
+    </div>
+  );
+}
+
+function LoginForm() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [userError, setUserError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const submitButtonRef = useRef(null);
+
+  function handleUser(e) {
+    setUser(e.target.value);
+    setUserError(user === "Haris" ? true : false);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleLoginSubmit(e) {
+    e.preventDefault();
+    if (user !== "Haris") {
+      setUserError(true);
+    } else setUserError(false);
+
+    if (Number(password) !== 1846) {
+      setPasswordError(true);
+    } else setPasswordError(false);
+
+    if (user === "Haris" && Number(password) === 1846) {
+      alert("Uspjesan login");
+    }
+
+    e.target.querySelectorAll("input").forEach((input) => input.blur());
+
+    setUser("");
+    setPassword("");
+    console.log(typeof password);
+  }
+
+  function handleAutoComplete() {
+    setUser("Haris");
+    setPassword(1846);
+  }
+
+  return (
+    <form className="login-form" onSubmit={handleLoginSubmit}>
+      <img
+        className="logo"
+        src="/Hotel_Flow__1_-removebg-preview.png"
+        alt="App Logo"
+      />
+      {/* <h3 className="login-title">Login</h3> */}
+      <input
+        className="input-field"
+        type="text"
+        placeholder="Benutzername"
+        value={user}
+        onChange={handleUser}
+        style={userError ? { border: "1.5px solid red" } : {}}
+      />
+      <input
+        className="input-field"
+        type="password"
+        placeholder="Kenwort"
+        value={password}
+        onChange={handlePassword}
+        style={passwordError ? { border: "1.5px solid red" } : {}}
+      />
+      <button className="btn btn-login">Anmelden</button>
+      <div className="user-data-login-container">
+        <ul className="user-data-login">
+          <li>Bitte folgende Daten verwenden:</li>
+          <li>Haris</li>
+          <li>1846</li>
+          <li>
+            oder
+            <strong className="redirection" onClick={handleAutoComplete}>
+              {" "}
+              hier{" "}
+            </strong>
+            klicken
+          </li>
+        </ul>
+      </div>
+    </form>
+  );
+}
